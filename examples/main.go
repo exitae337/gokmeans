f221a571174d75eac99326888bb9f3710e942de7
exitae337 <exitae337@gmail.com>
2025-03-18 18:02:06 +0300

    Questions with reflect

--- examples/main.go
+++ examples/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	moduleName := "GoKmeans: "
-	if ok, err := gokmeans.KmeansGo("../../storage/file.exel", 6, 4); err != nil {
+	if ok, err := gokmeans.KmeansGo("../../storage/file.exel", 6, 3); err != nil {
 		log.Fatal(moduleName, err)
 	} else {
 		fmt.Println(ok)
