ee75be61a89141a0feebdcac16382249eebc7910
exitae337 <exitae337@gmail.com>
2025-03-18 17:29:14 +0300

    Kmeans Go. Start

--- examples/main.go
+++ examples/main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	gokmeans "github.com/exitae337/gokmeans/lib/kmeans"
 )
 
 func main() {
-	gokmeans.KmeansGo()
+	moduleName := "GoKmeans: "
+	if ok, err := gokmeans.KmeansGo("../../storage/file.exel", 6, 4); err != nil {
+		log.Fatal(moduleName, err)
+	} else {
+		fmt.Println(ok)
+	}
 }
