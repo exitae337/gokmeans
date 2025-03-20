ff90c78b5a620c6c05622b176265b2dcee8e8cbb
exitae337 <exitae337@gmail.com>
2025-03-20 11:51:07 +0300

    New Point struct

--- lib/kmeans/kmeans.go
+++ lib/kmeans/kmeans.go
@@ -2,33 +2,12 @@ package kmeans
 
 import (
 	"fmt"
-	"reflect"
 )
 
-// Generics for distsnce between Points function
-type Point interface {
-	pointForCube | pointForSquare | pointForSegment
-}
-
-// Three measurements
-type pointForCube struct {
-	CoordX float64
-	CoordY float64
-	CoordZ float64
-	Metka  int
-}
-
-// Two measurements
-type pointForSquare struct {
-	CoordX float64
-	CoordY float64
-	Metka  int
-}
-
-// One measurement
-type pointForSegment struct {
-	CoordX float64
-	Metka  int
+// Point struct
+type Point struct {
+	PointValues  []float64
+	MetkaKlaster int
 }
 
 // Function
@@ -37,22 +16,9 @@ func KmeansGo(pathToFile string, k, measurements int) (bool, error) {
 		return false, fmt.Errorf("count of measurements must be in [1..3]")
 	}
 	fmt.Printf("Path to file: %v, K: %v, Measurements count for point: %v\n", pathToFile, k, measurements)
-	distanceBetween(pointForCube{}, pointForCube{})
 	return true, nil
 }
 
-func distanceBetween[T Point](firstPoint, secondPoint T) float64 {
-	pointType := reflect.TypeOf(firstPoint)
-	_ = secondPoint
-	switch pointType {
-	case reflect.TypeOf(pointForCube{}):
-		fmt.Println("cube value")
-	case reflect.TypeOf(pointForSquare{}):
-		fmt.Println("square value")
-	case reflect.TypeOf(pointForSegment{}):
-		fmt.Println("segment value")
-	default:
-		fmt.Println("unknown value")
-	}
+func distanceBetween(firstPoint, secondPoint Point) float64 {
 	return 0
 }
