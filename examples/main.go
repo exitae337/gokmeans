aa4bbe531c610408e63a2a7b1fb8238fc19d434d
exitae337 <exitae337@gmail.com>
2025-05-07 13:24:56 +0300

    Tests

--- examples/main.go
+++ examples/main.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+
+	"github.com/xuri/excelize/v2"
 
 	gokmeans "github.com/exitae337/gokmeans/lib/kmeans"
 )
 
 func main() {
+	createTestFile()
+	demoKmeans()
+}
+
+func demoKmeans() {
 	moduleName := "GoKmeans: "
-	clusters, err := gokmeans.KmeansGo("points.xlsx", "Sheet1", 4, 100, 0.001, true, 6)
+	clusters, err := gokmeans.KmeansGo("points.xlsx", "Sheet1", 8, 100, 0.001, false, 0)
 	if err != nil {
 		fmt.Println(moduleName, " : ", err)
 	}
@@ -17,4 +25,31 @@ func main() {
 		fmt.Printf("Centroid: %v\n", cluster.Centroid)
 		fmt.Printf("Points: %v\n\n", cluster.ClusterPoints)
 	}
+}
+
+func createTestFile() {
+	f := excelize.NewFile()
+	defer f.Close()
+
+	sheetName := "Sheet1"
+
+	numPoints := 100000
+	for row := 2; row <= numPoints+1; row++ {
+		x := rand.Float64() * 1000
+		y := rand.Float64() * 1000
+		z := rand.Float64() * 1000
+
+		// Записываем в колонки A, B, C (X, Y, Z)
+		f.SetCellValue(sheetName, fmt.Sprintf("A%d", row), x)
+		f.SetCellValue(sheetName, fmt.Sprintf("B%d", row), y)
+		f.SetCellValue(sheetName, fmt.Sprintf("C%d", row), z)
+	}
+
+	// Сохраняем файл
+	if err := f.SaveAs("points.xlsx"); err != nil {
+		fmt.Println("Failed to save test file:", err)
+		return
+	}
+
+	fmt.Println("File created: points.xlsx")
 }
---
+++ tests/kmeans_bench_test.go
@@ -0,0 +1,33 @@
+package kmeans_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/exitae337/gokmeans/lib/kmeans"
+)
+
+func BenchmarkKmeansGo_Classic(b *testing.B) {
+	benchmarkKmeansGo(b, 8, 1000, 0.001, false, 0)
+}
+
+func BenchmarkKmeansGo_KmeansPP(b *testing.B) {
+	benchmarkKmeansGo(b, 8, 1000, 0.001, true, 0) // k=8
+}
+
+func BenchmarkKmeansGo_KmeansMiniBatch(b *testing.B) {
+	benchmarkKmeansGo(b, 8, 1000, 0.001, true, 1000)
+}
+
+// B / op : bytes for operation
+// allocs / op: allocations
+func benchmarkKmeansGo(b *testing.B, k int, max_iterations int, threshold float64, kmeansPP bool, batch_size int) {
+	// Засекаем общее время выполнения всех итераций
+	startTime := time.Now()
+	for i := 0; i < b.N; i++ {
+		_, _ = kmeans.KmeansGo("../examples/points.xlsx", "Sheet1", k, max_iterations, threshold, kmeansPP, batch_size)
+	}
+	total := time.Since(startTime)
+	b.ReportMetric(float64(total.Milliseconds())/float64(b.N), "ms/op(total)")
+	b.ReportMetric(float64(total.Seconds()), "total_sec")
+}
