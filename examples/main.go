7fd99b027f2b1a7452f9125417462ea1101a793a
exitae337 <exitae337@gmail.com>
2025-05-21 12:52:57 +0300

    Demo examples change

--- examples/main.go
+++ examples/main.go
@@ -10,10 +10,11 @@ import (
 )
 
 func main() {
-	createTestFile()
+	// createTestFile()
 	demoKmeans()
 }
 
+// Classic K-means example
 func demoKmeans() {
 	moduleName := "GoKmeans: "
 	clusters, err := gokmeans.KmeansGo("points.xlsx", "Sheet1", 8, 100, 0.001, false, 0)
@@ -27,13 +28,42 @@ func demoKmeans() {
 	}
 }
 
+// Kmeans with kmeans++ init example
+func demoKmeansWithInitPlus() {
+	moduleName := "GoKmeans: "
+	clusters, err := gokmeans.KmeansGo("points.xlsx", "Sheet1", 8, 100, 0.001, true, 0)
+	if err != nil {
+		fmt.Println(moduleName, " : ", err)
+	}
+	for i, cluster := range clusters {
+		fmt.Printf("Cluster %d:\n", i+1)
+		fmt.Printf("Centroid: %v\n", cluster.Centroid)
+		fmt.Printf("Points: %v\n\n", cluster.ClusterPoints)
+	}
+}
+
+// Mini-batch K-means with k-means++ example
+func demoKmeansWithInitPlusAndBatches() {
+	moduleName := "GoKmeans: "
+	clusters, err := gokmeans.KmeansGo("points.xlsx", "Sheet1", 8, 100, 0.001, true, 100)
+	if err != nil {
+		fmt.Println(moduleName, " : ", err)
+	}
+	for i, cluster := range clusters {
+		fmt.Printf("Cluster %d:\n", i+1)
+		fmt.Printf("Centroid: %v\n", cluster.Centroid)
+		fmt.Printf("Points: %v\n\n", cluster.ClusterPoints)
+	}
+}
+
+// Creating test "Example File" .xslx for testing and working example
 func createTestFile() {
 	f := excelize.NewFile()
 	defer f.Close()
 
 	sheetName := "Sheet1"
 
-	numPoints := 100000
+	numPoints := 1000 // Number of points in data for clastering
 	for row := 2; row <= numPoints+1; row++ {
 		x := rand.Float64() * 1000
 		y := rand.Float64() * 1000
